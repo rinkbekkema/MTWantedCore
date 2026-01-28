113016d5190db2d05a2bb5768512162832314e55
rinkbekkema <140434924+rinkbekkema@users.noreply.github.com>
2026-01-28 17:34:51 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "nl.mtwanted"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
